47855f2a949c0e1dde372b45847d20d3e7aec0ed
Fekhreddine23 <fekhredine1990@gmail.com>
2023-01-25 10:10:12 +0100

    initialisation du projet fil rouge en spring boot

---
+++ src/main/java/fr/diginamic/GP3Covoiturage/Gp3CovoiturageApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.GP3Covoiturage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gp3CovoiturageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gp3CovoiturageApplication.class, args);
+	}
+
+}
