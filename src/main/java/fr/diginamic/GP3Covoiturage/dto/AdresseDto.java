09e2ec8229a6a1f0b45d7f5c4df4a0e343448eab
Saranthony <Saranthony@DESKTOP-VSCEOE9.>
2023-01-26 14:38:52 +0100

    Suppression imports inutiles

--- src/main/java/fr/diginamic/GP3Covoiturage/dto/AdresseDto.java
+++ src/main/java/fr/diginamic/GP3Covoiturage/dto/AdresseDto.java
@@ -6,17 +6,6 @@ package fr.diginamic.GP3Covoiturage.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 /**
  * @author antPinot
  * 
