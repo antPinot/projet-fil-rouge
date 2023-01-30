0ba698df2d37908eddaeb1fc01be2abff6e80a1b
Fekhreddine23 <fekhredine1990@gmail.com>
2023-01-30 12:29:51 +0100

    ajout method mapper sur les ctrls et services class covoiturafe et role

--- src/main/java/fr/diginamic/GP3Covoiturage/services/RoleService.java
+++ src/main/java/fr/diginamic/GP3Covoiturage/services/RoleService.java
@@ -3,8 +3,12 @@ package fr.diginamic.GP3Covoiturage.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+import fr.diginamic.GP3Covoiturage.dto.CovoiturageDtoMapper;
+import fr.diginamic.GP3Covoiturage.dto.RoleDto;
+import fr.diginamic.GP3Covoiturage.dto.RoleDtoMapper;
+import fr.diginamic.GP3Covoiturage.models.Covoiturage;
 import fr.diginamic.GP3Covoiturage.models.Role;
 import fr.diginamic.GP3Covoiturage.repositories.RoleRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -13,40 +17,58 @@ import jakarta.validation.Valid;
 /**
  * @author Fekhreddine
  */
+
+@Service
 public class RoleService {
 
 	@Autowired
 	RoleRepository roleRepository;
 
 	/**
-	 * @method services de CRUD basique
+	 * @method create()
 	 */
-
 	public Role create(@Valid Role createRole) {
 
 		if (createRole.getId() != null) {
 			throw new RuntimeException("erreur : id role existe deja present");
 		}
+
+		 
 		return this.roleRepository.save(createRole);
+
 	}
 
+	/**
+	 * @method update()
+	 */
 	public Role update(@Valid Role updateRole) {
 
 		if (updateRole.getId() == null) {
 
 			throw new RuntimeException("erreur : le role n'est pas encore créer");
 		}
+ 
 		return this.roleRepository.save(updateRole);
+
 	}
 
+	/**
+	 * @method findAll()
+	 */
 	public List<Role> findAll() {
 		return roleRepository.findAll();
 	}
 
+	/**
+	 * @method findById()
+	 */
 	public Role findById(Integer id) {
 		return this.roleRepository.findById(id).orElseThrow(EntityNotFoundException::new);
 	}
 
+	/**
+	 * @method delete()
+	 */
 	public void delete(Integer id) {
 
 		if (id == null) {
